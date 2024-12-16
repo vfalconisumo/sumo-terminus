c3413b4a33cba7aaeb02e1653388f99a864df9c1
Greg Anderson <greg@pantheon.io>
2024-12-16 12:28:56 -0800

    [DEVX-3831] PHP 8.4 Compatibility; removes support for PHP 8.1 and earlier. (#2655)

--- src/Terminus.php
+++ src/Terminus.php
@@ -179,7 +179,7 @@ EOD;
 
         // Add the services
         // Request
-        $container->share('request', Request::class);
+        $container->addShared('request', Request::class);
         $container->inflector(RequestAwareInterface::class)
             ->invokeMethod('setRequest', ['request']);
 
@@ -188,7 +188,7 @@ EOD;
             $this->getConfig()->get('cache_dir')
         );
         $session = new Session($session_store);
-        $container->share('session', $session);
+        $container->addShared('session', $session);
         $container->inflector(SessionAwareInterface::class)
             ->invokeMethod('setSession', ['session']);
 
@@ -210,7 +210,7 @@ EOD;
         $container->add(PluginDiscovery::class);
         $container->add(PluginInfo::class);
 
-        $container->share('sites', Sites::class);
+        $container->addShared('sites', Sites::class);
         $container->inflector(SiteAwareInterface::class)
             ->invokeMethod('setSites', ['sites']);
 
@@ -493,7 +493,7 @@ EOD;
      *
      * @return integer $status_code The exiting status code of the application
      */
-    public function run(InputInterface $input = null, OutputInterface $output = null)
+    public function run(?InputInterface $input = null, ?OutputInterface $output = null)
     {
         if ($input === null) {
             $input = $this->input();
